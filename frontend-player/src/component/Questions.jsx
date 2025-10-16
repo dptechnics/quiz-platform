119322114d2c66fd9bd2a3bb366fd91c0f1f82a4
Daan Pape <daan@dptechnics.com>
2025-10-16 14:55:38 +0200

    Quiz design is starting to take shape

--- frontend-player/src/component/Questions.jsx
+++ frontend-player/src/component/Questions.jsx
@@ -2,35 +2,42 @@ import { observer } from "mobx-react-lite";
 import { quizStore } from "../store/QuizStore";
 
 export const Questions = observer(() => (
-  <div className="card">
-    <p>
-      {quizStore.quiz.currentQuestion.question}
-    </p>
-    {
-      quizStore.quiz.currentQuestion.type === "multiplechoice" && (
-        <>
-          {quizStore.quiz.currentQuestion.choices.map((choice, idx) => (
-            <p key={idx}>
-              <input 
-                type="radio"
-                name="answers" 
-                value={idx}
-                checked={quizStore.currentAnswer === idx}
-                onChange={() => quizStore.setAnswer(idx)}
-              />
-              <label htmlFor={`choice-${idx}`}>{choice}</label>
-            </p>
-          ))}
-          <button onClick={() => quizStore.sendAnswer()}>Send answer</button>
-        </>
-      )
-    }
-    {
-      quizStore.quiz.currentQuestion.type == "value" &&
-      <>
-        <input type="number" onChange={ (e) => quizStore.setAnswer(e.target.value) }/>
-        <button onClick={() => quizStore.sendAnswer()}>Send answer</button>
-      </>
+  <div className="data">
+    <div className="dataContent">
+      <div className="questioncontent">
+        <p>
+          {quizStore.quiz.currentQuestion.question}
+        </p>
+        {
+          quizStore.quiz.currentQuestion.type === "multiplechoice" && (
+            <>
+              {quizStore.quiz.currentQuestion.choices.map((choice, idx) => (
+                <p key={idx}>
+                  <input
+                    type="radio"
+                    name="answers"
+                    value={idx}
+                    checked={quizStore.currentAnswer === idx}
+                    onChange={() => quizStore.setAnswer(idx)}
+                  />
+                  <label htmlFor={`choice-${idx}`}>{choice}</label>
+                </p>
+              ))}
+            </>
+          )
+        }
+        {
+          quizStore.quiz.currentQuestion.type == "value" &&
+          <>
+            <input type="number" onChange={(e) => quizStore.setAnswer(e.target.value)} />
+          </>
+        }
+      </div>
+    </div>
+    {quizStore.quiz.currentQuestion.id >= 0 &&
+      <button className="button" onClick={() => quizStore.sendAnswer()}>
+        Send answer
+      </button>
     }
   </div>
 ));
--- frontend-player/src/component/Register.jsx
+++ frontend-player/src/component/Register.jsx
@@ -2,13 +2,14 @@ import { observer } from "mobx-react-lite";
 import { quizStore } from "../store/QuizStore";
 
 export const Register = observer(() => (
-  <div className="register">
-    <p>
-      Click register to take part in the Quiz.
+  <div className="data">
+    <p className="dataContent">
+      Click 'Start playing' to take part in the quiz. Your player's name will be automatically
+      assigned and is visible at the top of the screen.
     </p>
-    
+
     <button className="button" onClick={() => quizStore.register()}>
-      <span>Register</span>
+      Start playing
     </button>
   </div>
 ));
