c47a96a11a0b6622a41ee87f9143d2df00062227
Daan Pape <daan@dptechnics.com>
2025-10-28 21:47:45 +0100

    Updated the presenter view

--- frontend-presenter/src/component/Footer.jsx
+++ frontend-presenter/src/component/Footer.jsx
@@ -1,9 +1,16 @@
 import { observer } from "mobx-react-lite";
 import dptLogo from '../assets/logo-dptechnics.svg';
+import quickspotLogo from '../assets/logo-quickspot.svg';
+import bluecherryLogo from '../assets/logo-bluecherry.svg';
 
 export const Footer = observer(() => (
   <section className="footer">
-    <p>Welcome at booth 6047</p>
-    <img src={dptLogo} alt="DPTechnics logo" />
+    <p>Discover multi-radio IoT (LTE-M, NB-IoT, WiFi, BLE, GNSS) in a single module at booth 6047</p>
+    <div className="footerImages">
+      <img src={quickspotLogo} alt="Quickspot logo" />
+      <img src={dptLogo} alt="DPTechnics logo" />
+      <img src={bluecherryLogo} alt="BlueCherry logo" />
+    </div>
+    
   </section>
 ));
